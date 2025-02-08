f6d7888a0451fcdce5d47eaff04e7ccf6f7bd37f
Jobin Aj <jobinaj6361@gmail.com>
2025-02-08 13:18:34 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import '../tracing.js'; // 👈 Add OpenTelemetry tracing before anything else
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 import { Provider } from "react-redux";
 import Store from './app/Store.js';
 import { Toaster } from 'react-hot-toast';
@@ -9,8 +10,8 @@ import { Toaster } from 'react-hot-toast';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={Store}>
-    <Toaster position='top-center' reverseOrder={false} />
-      <App/>
+      <Toaster position='top-center' reverseOrder={false} />
+      <App />
     </Provider>
   </React.StrictMode>
-)
+);
